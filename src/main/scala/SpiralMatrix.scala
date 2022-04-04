14eefa845a71f2447ef60d70317320bc23c0cf0f
a_szczepanik <a.szczepanik@adform.com>
2022-04-04 11:37:38 +0200

    Update on metod nextDirection

--- src/main/scala/SpiralMatrix.scala
+++ src/main/scala/SpiralMatrix.scala
@@ -19,31 +19,4 @@ object SpiralMatrix extends App {
     case Left => Up
   }
 
-  def spiralMatrix(steps: Int) = {
-    var step = 0
-    var x = 0
-    var y = 0
-    var maxSteps = steps
-    var direction: Direction = Right
-    val spiral: mutable.Map[(Int, Int), Int] = mutable.Map()
-
-    for (i <- 1 to (steps * steps)) {
-      step += 1
-      if (step >= maxSteps) {
-        direction = nextDirection(direction)
-        if (direction == Up || direction == Down) {
-          maxSteps -= 1
-        }
-        step = 0
-      }
-
-      spiral += ((x, y) -> i)
-      direction match {
-        case Right => x += 1
-        case Down => y -= 1
-        case Left => x -= 1
-        case Up => y += 1
-      }
-    }
-  }
 }
