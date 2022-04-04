a768ef9ba71d60d7179e38435ce90b39db24bdbe
a_szczepanik <a.szczepanik@adform.com>
2022-04-04 10:49:39 +0200

    Create for comprehension for changing directions

--- src/main/scala/SpiralMatrix.scala
+++ src/main/scala/SpiralMatrix.scala
@@ -3,20 +3,40 @@ import scala.collection.mutable
 sealed trait Direction
 
 object Up extends Direction
+
 object Right extends Direction
+
 object Down extends Direction
+
 object Left extends Direction
 
 object SpiralMatrix extends App {
 
-  def spiralMatrix(steps: Int): List[List[Int]] = {
-    val step = 0
-    val x = 0
-    val y = 0
-    val maxSteps = steps
-    val direction: Direction = Right
-    val spiral: mutable.Map[(Int, Int), Int] = mutable.Map()
+  def nextDirection(direction: Direction): Direction = direction match {
+    case Up => Right
+    case Right => Down
+    case Down => Left
+    case Left => Up
+  }
 
+  def spiralMatrix(steps: Int): List[List[Int]] = {
+    var step = 0
+    var x = 0
+    var y = 0
+    var maxSteps = steps
+    var direction: Direction = Right
+    var spiral: mutable.Map[(Int, Int), Int] = mutable.Map()
+
+    for (i <- 1 to (steps * steps)) {
+      step += 1
+      if (step >= maxSteps) {
+        direction = nextDirection(direction)
+        if (direction == Up || direction == Down) {
+          maxSteps -= 1
+        }
+        step = 0
+      }
+    }
   }
 
 }
